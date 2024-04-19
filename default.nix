f691e3cbf52f5e0043593c4869aad2e536f5244b
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-04-19 15:36:39 +0800

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
-{ pkgs ? import <nixpkgs> { system = builtins.currentSystem } }: {
+{ pkgs ? import <nixpkgs> { system = builtins.currentSystem }; }: {
   nbdkit = pkgs.callPackage ./nbdkit.nix {};
 }
