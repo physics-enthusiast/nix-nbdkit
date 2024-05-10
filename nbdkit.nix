d930e236a1534981932b94c10683ac52868ba016
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-05-10 14:54:38 +0800

    Update nbdkit.nix

--- nbdkit.nix
+++ nbdkit.nix
@@ -50,7 +50,7 @@ stdenv.mkDerivation ({
     ++ lib.optionals tlsSupport [ gnutls ];
 
   postPatch = ''
-    patchShebangs --build $PWD
+    patchShebangs --build ./
   '' + lib.optionals ocamlPluginSupport ''
     sed -i plugins/ocaml/Makefile.am -e "s|\$(OCAMLLIB)|\"$out/lib/ocaml/${ocaml.version}/site-lib/\"|g"
   '';
