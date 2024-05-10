a8b70ea0c735a0d420a690dc70f6ee3ca65b10a0
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-05-10 22:32:37 +0800

    Update nbdkit.nix

--- nbdkit.nix
+++ nbdkit.nix
@@ -10,7 +10,7 @@
 , pythonPluginSupport ? true, python3
 , rustPluginSupport ? true, rustc, rustPlatform, cargo
 , tclPluginSupport ? true, tcl
-, additionalOptionalPlugins ? true, curl, libguestfs-with-appliance, libisoburn, libvirt, e2fsprogs, libnbd, libssh, libtorrent-rasterbar, boost, qemu
+, additionalOptionalPlugins ? true, curl, libguestfs, libisoburn, libvirt, e2fsprogs, libnbd, libssh, libtorrent-rasterbar, boost, qemu
 , enableManpages ? true
 }: 
 let
@@ -51,7 +51,7 @@ stdenv.mkDerivation ({
     ++ lib.optionals enableManpages [ (perl.withPackages (p: [ p.PodSimple ])) ]
     ++ lib.optionals selinuxSupport [ libselinux ]
     ++ lib.optionals tlsSupport [ gnutls ]
-    ++ lib.optionals additionalOptionalPlugins [ curl libguestfs-with-appliance libisoburn libvirt e2fsprogs libnbd libssh libtorrent-rasterbar boost ];
+    ++ lib.optionals additionalOptionalPlugins [ curl libguestfs libisoburn libvirt e2fsprogs libnbd libssh libtorrent-rasterbar boost ];
 
   nativeCheckInputs = []
     ++ lib.optionals additionalOptionalPlugins [ qemu ];
@@ -91,6 +91,7 @@ stdenv.mkDerivation ({
   configureFlags = [
     # Diagnostic info requested by upstream
     "--with-extra='Nixpkgs'"
+    # Same problem as #37540, would rather not bundle a downloaded binary so just disable the test instead
     "--disable-libguestfs-tests"
   ] 
     # Most language plugins are automatically turned on or off based on the
