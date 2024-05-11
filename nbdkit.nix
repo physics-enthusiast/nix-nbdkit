31c8e4a9b4334aabeb2778402daff96198c13076
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-05-12 00:59:12 +0800

    Update nbdkit.nix

--- nbdkit.nix
+++ nbdkit.nix
@@ -55,18 +55,18 @@ stdenv.mkDerivation ({
     ++ lib.optionals tlsSupport [ gnutls ]
     ++ lib.optionals additionalOptionalFeatures [ curl libguestfs libisoburn libvirt e2fsprogs libnbd libssh libtorrent-rasterbar boost lzma zlib-ng ];
 
-  postUnpack = "" + lib.optionals goPluginSupport ''
+  postUnpack = lib.optionalString goPluginSupport ''
     export GOCACHE=$TMPDIR/go-cache
     export GOPATH="$TMPDIR/go"
     export GOPROXY=off
     go run source/plugins/golang/config-test.go
-  '' + lib.optionals rustPluginSupport ''
+  '' + lib.optionalString rustPluginSupport ''
     cp source/plugins/rust/Cargo.lock.msrv source/plugins/rust/Cargo.lock
   ''; 
 
-  postPatch = "" + lib.optionals ocamlPluginSupport ''
+  postPatch = lib.optionalString ocamlPluginSupport ''
     sed -i plugins/ocaml/Makefile.am -e "s|\$(OCAMLLIB)|\"$out/lib/ocaml/${ocaml.version}/site-lib/\"|g"
-  '' + lib.optionals stdenv.isDarwin ''
+  '' + lib.optionalString stdenv.isDarwin ''
     rm plugins/example3/Makefile.am
     touch plugins/example3/Makefile.am
   '';
