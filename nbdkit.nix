e2857d68a0724b2c548542c79cc5766b9d81a2cf
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-05-12 19:21:51 +0800

    Update nbdkit.nix

--- nbdkit.nix
+++ nbdkit.nix
@@ -89,7 +89,9 @@ stdenv.mkDerivation ({
       # directly exit successfully. See the comments on --disable-libguestfs-tests for more details
       substituteInPlace "$test_file" \
         --replace-quiet '/usr/bin/env bash' '${bash}/bin/bash' \
-        --replace-quiet 'requires guestfish --version' 'exit 0'
+        --replace-quiet 'out=' 'other_out=' \
+        --replace-quiet '$out' '$other_out' \
+        --replace-quiet 'requires guestfish --version' 'exit 0' \
     done
   '';
 
