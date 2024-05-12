16c5a75ed59594937c7aac622f6ba55e0f571697
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-05-12 17:36:23 +0800

    Update nbdkit.nix

--- nbdkit.nix
+++ nbdkit.nix
@@ -85,7 +85,8 @@ stdenv.mkDerivation ({
       # directly exit successfully. See the comments on --disable-libguestfs-tests for more details
       substituteInPlace "$test_file" \
         --replace-quiet '/usr/bin/env bash' '${bash}/bin/bash' \
-        --replace-quiet 'requires guestfish --version' 'exit 0'
+        --replace-quiet 'requires guestfish --version' 'exit 0' \
+        || echo "Binary detected, skipping"
     done
   '';
 
