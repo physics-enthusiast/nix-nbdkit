41c9fd084c254a363f1485360a5c90f27141ad3b
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-05-13 15:43:20 +0800

    Update nbdkit.nix

--- nbdkit.nix
+++ nbdkit.nix
@@ -1,4 +1,4 @@
-{ lib, stdenv, bash, autoreconfHook, pkg-config, which
+{ gcc9, lib, stdenv, bash, autoreconfHook, pkg-config, which
 , fetchFromGitLab
 , runCommand
 , completionSupport ? true, bash-completion
@@ -69,6 +69,7 @@ stdenv.mkDerivation ({
     cp source/plugins/rust/Cargo.lock.msrv source/plugins/rust/Cargo.lock
   '' + ''
     echo 'print_endline "test"' > conftest.ml
+    export OCAMLOPTFLAGS="-cc ${gcc9}/bin/cc"
     ocamlopt -verbose -S -output-obj -runtime-variant _pic -o conftest.so conftest.ml || { cat conftest.s; ${stdenv.cc}/bin/cc -c -o 'conftest.o' 'conftest.s'; }
   '' ; 
 
