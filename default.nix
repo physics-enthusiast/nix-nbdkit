78e61588deb69d033bb644722554300caab360b0
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-05-13 19:02:03 +0800

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,14 @@
 { pkgs ? import <nixpkgs> {} }: rec {
+  memorymapping = pkgs.callPackage ./memorymapping { };
+  memorymappingHook = pkgs.makeSetupHook {
+    name = "memorymapping-hook";
+    propagatedBuildInputs = [ memorymapping ];
+  } ./memorymapping/setup-hook.sh;
+  memstream = pkgs.callPackage ./memstream { };
+  memstreamHook = pkgs.makeSetupHook {
+    name = "memstream-hook";
+    propagatedBuildInputs = [ memstream ];
+  } ./memstream/setup-hook.sh;
   libnbd = pkgs.callPackage ./libnbd.nix {};
-  nbdkit = pkgs.callPackage ./nbdkit.nix { inherit libnbd; };
+  nbdkit = pkgs.callPackage ./nbdkit.nix { inherit memorymappingHook memstreamHook libnbd; };
 }
