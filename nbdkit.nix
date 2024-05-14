3dbc9b457347d07986e6d7ac1902ff1722409060
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2024-05-14 15:27:31 +0800

    Update nbdkit.nix

--- nbdkit.nix
+++ nbdkit.nix
@@ -101,8 +101,8 @@ stdenv.mkDerivation ({
   '';
 
   postBuild = lib.optionalString stdenv.isDarwin ''
-    mv $out/sbin/nbdkit $out/sbin/nbdkit-unwrapped
-    cat ${ocamlPackages.ocaml}/lib/ocaml/libasmrun_pic.a |grep startup
+    cat ${ocamlPackages.ocaml}/lib/ocaml/libasmrun_pic.a | grep startup
+    mv $out/bin/nbdkit $out/bin/nbdkit-unwrapped
     makeWrapper $out/bin/nbdkit-unwrapped $out/bin/nbdkit \
       --prefix DYLD_LIBRARY_PATH : "${ocamlPackages.ocaml}/lib/ocaml"
   '';
